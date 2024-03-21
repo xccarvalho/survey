c2759feb52c266412f16acbd313d9a68c25f041e
Caroll Carvalho <xccaroll@hotmail.com>
2024-03-21 19:02:23 -0300

    updated button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,36 +1,28 @@
-import { MaterialIcons } from "@expo/vector-icons";
 import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
 
 type Props = TouchableOpacityProps & {
+  variant?: "text" | "contained";
   title: string;
   lgButton?: boolean;
-  backButton?: boolean;
-  handleBack?: () => void;
 };
 
 export function Button({
+  variant = "text",
   title,
   lgButton = false,
-  backButton = false,
-  handleBack,
   ...rest
 }: Props) {
-  return backButton ? (
-    <TouchableOpacity
-      className="flex flex-row items-center ml-3 mt-2"
-      onPress={handleBack}
-    >
-      <MaterialIcons name="chevron-left" size={26} className="text-green-950" />
-      <Text>{title}</Text>
-    </TouchableOpacity>
-  ) : (
+  return (
     <TouchableOpacity
       activeOpacity={0.7}
-      className={`bg-green-950 rounded-lg items-center justify-center 
-       ${lgButton ? "py-7 px-7" : "px-4 py-4"}`}
+      className={`items-center justify-center rounded-lg 
+      ${variant === "contained" && "bg-green-950"}
+      ${lgButton ? "px-7 py-7" : "px-4 py-4"}`}
       {...rest}
     >
-      <Text className="text-white text-sm font-semiBold uppercase">
+      <Text
+        className={`font-bold text-sm uppercase  ${variant === "contained" ? "text-white" : "text-green-950"}`}
+      >
         {title}
       </Text>
     </TouchableOpacity>
