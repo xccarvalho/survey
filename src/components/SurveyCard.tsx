e9acf7bab310ac123c68953b24625e12601e1a0f
Caroll Carvalho <xccaroll@hotmail.com>
2024-03-24 03:48:37 -0300

    updated SurveyCard, SurveyList

--- src/components/SurveyCard.tsx
+++ src/components/SurveyCard.tsx
@@ -1,27 +1,83 @@
-import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
+import { Text, View, PressableProps, Pressable } from "react-native";
+import { IconButton } from "./IconButton";
+import classNames from "classnames";
 
 export type SurveyProps = {
   title: string;
   description: string;
+  isClosed?: boolean;
+  isAdm?: boolean;
+  questions?: [];
+  handlePress?: () => void;
+  handleDeleteSurvey?: () => void;
+  handleEditSurvey?: () => void;
+  handleCloseSurvey?: () => void;
 };
 
-type Props = TouchableOpacityProps & {
+type Props = PressableProps & {
   survey: SurveyProps;
 };
 
 export function SurveyCard({ survey, ...rest }: Props) {
   return (
-    <TouchableOpacity
-      className="h-24 rounded-lg border-none bg-green-50 p-4 shadow-md"
-      activeOpacity={0.7}
+    <Pressable
+      style={({ pressed }) => [{ opacity: pressed ? 0.7 : 1 }]}
+      className={classNames(
+        "shadow-green-4 max-h-40 gap-3 rounded-lg bg-zinc-100 p-4 shadow",
+        { "bg-yellow-900": survey.isClosed },
+      )}
+      onPress={survey.handlePress}
       {...rest}
     >
-      <Text className="mb-1 font-bold text-lg text-green-950">
-        {survey.title}
-      </Text>
-      <Text className="truncate font-regular text-sm text-gray-700">
-        {survey.description}
-      </Text>
-    </TouchableOpacity>
+      <View>
+        <View
+          className={classNames({
+            "flex-row items-center justify-between leading-none": survey.isAdm,
+          })}
+        >
+          <Text
+            numberOfLines={1}
+            lineBreakMode="tail"
+            className="font-bold text-lg leading-none text-green-950"
+          >
+            {survey.title}
+          </Text>
+          {survey.isAdm ? (
+            <Text className="font-normal text-green-950">
+              {survey.questions?.length} answers
+            </Text>
+          ) : null}
+        </View>
+        <Text
+          numberOfLines={3}
+          lineBreakMode="tail"
+          className="mt-3 font-regular text-sm text-gray-700"
+        >
+          {survey.description}
+        </Text>
+      </View>
+      {survey.isAdm ? (
+        <View className="flex-row self-end">
+          <IconButton
+            iconName={"delete-forever"}
+            size={28}
+            color="rgb(220, 38, 38)"
+            onPress={survey.handleDeleteSurvey}
+          />
+          <IconButton
+            iconName={"edit"}
+            size={28}
+            color="rgb(34, 197, 94)"
+            onPress={survey.handleEditSurvey}
+          />
+          <IconButton
+            iconName={"cancel-presentation"}
+            size={28}
+            color="rgb(234, 179, 8)"
+            onPress={survey.handleCloseSurvey}
+          />
+        </View>
+      ) : null}
+    </Pressable>
   );
 }
--- src/components/SurveysList.tsx
+++ src/components/SurveysList.tsx
@@ -1,5 +1,7 @@
-import { ScrollView } from "react-native";
+import { Alert, ScrollView } from "react-native";
 import { SurveyCard } from "./SurveyCard";
+import { useState } from "react";
+import { ModalConfirm } from "./ModalConfirm";
 
 export type SurveyListProps = {
   id: string;
@@ -7,13 +9,32 @@ export type SurveyListProps = {
 }[];
 
 type Props = {
-  SurveyList?: SurveyListProps;
+  isAdmin?: boolean;
+  surveyList?: SurveyListProps;
 };
 
-export function SurveysList({ SurveyList, ...rest }: Props) {
+interface SurveyCardData {
+  id: string;
+  title: string;
+}
+
+export function SurveysList({ isAdmin, surveyList, ...rest }: Props) {
+  const [surveyCard, setSurveyCard] = useState<SurveyCardData[]>([]);
+  const [modalDeleteVisible, setModalDeleteVisible] = useState(false);
+  const [modalCloseVisible, setModalCloseVisible] = useState(false);
+
+  const handleDeleteSurvey = (id: number) => {
+    const updatedItems = surveyCard.filter((item) => Number(item.id) !== id);
+    setSurveyCard(updatedItems);
+  };
+
+  const handleCloseSurvey = (id: number) => {
+    Alert.alert("handleClose");
+  };
+
   return (
     <ScrollView
-      showsVerticalScrollIndicator={true}
+      showsVerticalScrollIndicator={false}
       contentContainerClassName="gap-4"
       className="mx-auto max-h-[37.5rem] w-11/12"
     >
@@ -21,17 +42,35 @@ export function SurveysList({ SurveyList, ...rest }: Props) {
         <Survey key={id} survey={{ title }} />
       ))} */}
 
-      {Array.from({ length: 45 }).map((_survey, index) => (
+      {Array.from({ length: 3 }).map((_survey, index) => (
         <SurveyCard
           {...rest}
           key={index}
           survey={{
             title: `${index + 1}  - Survey Title`,
             description:
-              "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+              "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+            isAdm: isAdmin,
+            handleDeleteSurvey: () => setModalDeleteVisible(true),
+            handleEditSurvey: () => console.log("handleEdit"),
+            handleCloseSurvey: () => console.log("handleClose"),
           }}
         />
       ))}
+      <ModalConfirm
+        textContent={"Would you like to delete it?"}
+        handleConfirm={handleDeleteSurvey}
+        modalVisible={modalDeleteVisible}
+        setModalVisible={() => setModalDeleteVisible(!modalDeleteVisible)}
+        onPressCancelBtn={() => setModalDeleteVisible(!modalDeleteVisible)}
+      />
+      <ModalConfirm
+        textContent={"Would you like to close it?"}
+        handleConfirm={handleCloseSurvey}
+        modalVisible={modalCloseVisible}
+        setModalVisible={() => setModalCloseVisible(!modalCloseVisible)}
+        onPressCancelBtn={() => setModalCloseVisible(!modalCloseVisible)}
+      />
     </ScrollView>
   );
 }
